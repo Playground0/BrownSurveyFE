9d9526041a595036c0b9bf9b623dee6f07f8fc72
Playground0 <emailplayg1@gmail.com>
2023-02-14 14:49:59 +0530

    remove sharedmodule import from public module in the import section

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { PublicRoutingModule } from './public-routing.module';
 import { PublicComponent } from './public.component';
-import { SharedModule } from '../shared/shared.module';
+
 
 
 @NgModule({
