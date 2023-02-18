59d8e10f8dc17f8775e6185203a5820ae814973b
Playground0 <emailplayg1@gmail.com>
2023-02-18 18:54:17 +0530

    Add Bootstrap and change component to components in Shared Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './shared/component/footer/footer.component';
-import { HeaderComponent } from './shared/component/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 import { SharedModule } from './shared/shared.module';
 
 
