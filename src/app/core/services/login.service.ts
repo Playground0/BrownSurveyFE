8f3146eba9ee503139147557e2d2fe4f7758f854
Playground0 <emailplayg1@gmail.com>
2023-09-21 16:12:28 +0530

    login changes

--- src/app/core/services/login.service.ts
+++ src/app/core/services/login.service.ts
@@ -1,9 +1,67 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, map } from 'rxjs';
+import { LoginModel, SignUpModel } from 'src/app/public/models/login';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  localHost : boolean = true;
+  apiUrl = this.localHost ? "http://localhost:8080/api" : "https://zany-cyan-puffer-slip.cyclic.app/api";
+  private userAuthDetails  = new BehaviorSubject<any>(null);
+  userAuthDetails$ = this.userAuthDetails.asObservable();
+
+  constructor(private http: HttpClient, private authService: AuthService) { }
+  login(loginDetails: LoginModel){
+    return this.http.post(`${this.apiUrl}/auth/login`,loginDetails).pipe(map((res) => {
+      this.setLocalData("authData",JSON.stringify(res));
+      this.setUsetAuthDetails(this.getLocalData("authData"));
+      return res;
+    }));
+  }
+  signUp(loginDetails: SignUpModel){
+    return this.http.post(`${this.apiUrl}/auth/register`,loginDetails).pipe(map((res) => {
+      this.setLocalData("authData",JSON.stringify(res));
+      this.setUsetAuthDetails(this.getLocalData("authData"));
+      return res;
+    }));;
+  }
+  logOut(id: string,email : string){
+    let equestObj = {
+      email : email
+    }
+    return this.http.patch(`${this.apiUrl}/auth/logout/${id}`,equestObj);
+  }
+  logOutUser(){
+    let userData = this.getLocalData("authData").Response;
+    this.logOut(userData.id,userData.email).subscribe({
+      next: (res:any) => {
+        if(res.Action_Status === "SUCCESS"){
+          this.removeLocalItem("authData");
+          this.setUsetAuthDetails(null);
+        }
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    })
+    
+  }
+  setLocalData(variableName:string, value:string){
+    localStorage.setItem(variableName,value);
+  }
+  getLocalData(variableName:string) : any{
+    let sessionData = localStorage.getItem(variableName);
+    let localValue:any = sessionData ? JSON.parse(sessionData) : "";
+    return localValue;
+  }
+  removeLocalItem(variableName:string){
+    localStorage.removeItem(variableName);
+  }
+  setUsetAuthDetails(value:any){
+    this.userAuthDetails.next(value);
+  }
 }
--- src/app/public/components/login/login.component.ts
+++ src/app/public/components/login/login.component.ts
@@ -1,10 +1,87 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { LoginModel, SignUpModel } from '../../models/login';
+import { PublicApiService } from '../../services/public-api.service';
+import { LoginService } from 'src/app/core/services/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  loginForm!:FormGroup;
+  loginErrMsg : string = "";
+  currentPageTitle: string = "Login";
+  toggleLoginPage: boolean = false;
+  constructor(private fb:FormBuilder, private loginService: LoginService,private router: Router){}
+  ngOnInit(): void {
+    this.loginForm = this.initializeForm();
+  }
+  initializeForm(){
+    return this.fb.group({
+      email: new FormControl('',Validators.required),
+      password: new FormControl('',Validators.required),
+      username: new FormControl(''),
+      confirmpassword: new FormControl('')
+    });
+  }
+  onLogin(){
+    let loginValue : LoginModel = this.loginForm.value;
+    this.loginErrMsg = "";
+    this.loginService.login(loginValue).subscribe({
+      next: (res:any) => {
+          this.router.navigateByUrl('/profile');
+      },
+      error: (err) => {
+        this.loginErrMsg = err.error.Action_Status;
+      }
+    });
+  }
+  onSignUp(){
+    let loginValue : SignUpModel = this.loginForm.value;
+    if(loginValue.password !== this.loginForm.value.confirmpassword){
+      this.loginErrMsg = "Password do not match";
+      return;
+    }
+    this.loginService.signUp(loginValue).subscribe({
+      next: (res:any) => {
+        this.toggleSignUpAndLogin();
+      },
+      error: (err) => {
+        this.loginErrMsg = err.error.Action_Status;
+      }
+    })
+  }
+  toggleSignUpAndLogin(){
+    // this.loginForm.reset();
+    this.loginErrMsg = "";
+    this.toggleLoginPage = !this.toggleLoginPage;
+    if(!this.toggleLoginPage){
+      this.currentPageTitle = "Login";
+      this.addOrRemoveControl(false);
+      return;
+    }
+    this.currentPageTitle = "Sign Up";
+    this.addOrRemoveControl(true);
+  }
+  addOrRemoveControl(addControl: boolean){
+    if(addControl){
+      this.loginForm.get('username')?.addValidators(Validators.required);
+      this.loginForm.get('confirmpassword')?.addValidators(Validators.required);
+      this.loginForm.updateValueAndValidity();
+      return
+    }
+    if(this.loginForm.get('username')?.hasValidator(Validators.required)){
+      this.loginForm.get('username')?.removeValidators(Validators.required);
+      this.loginForm.updateValueAndValidity();
+    }
+    if(this.loginForm.get('confirmpassword')?.hasValidator(Validators.required)){
+      this.loginForm.get('confirmpassword')?.removeValidators(Validators.required);
+      this.loginForm.updateValueAndValidity();
+    }
+    return;
+  }
 }
